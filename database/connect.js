5fae9da1abc3bc4f866084442006adec8c6c42bc
Oliver Y. Kui <oliverkui0324@outlook.com>
2024-02-17 23:05:29 +0800

    update favicon

--- database/connect.js
+++ database/connect.js
@@ -2,7 +2,8 @@ const pgp = require("pg-promise")();
 
 const password = process.env.PASSWORD;
 const db = pgp(
-  `postgresql://postgres:${password}@viaduct.proxy.rlwy.net:35556/railway`
+  // `postgresql://postgres:${password}@viaduct.proxy.rlwy.net:35556/railway`
+  `postgresql://postgres:${password}@postgres.railway.internal:5432/railway`
 );
 
 module.exports = db;
