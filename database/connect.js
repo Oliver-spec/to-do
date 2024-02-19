04d58a63ced666d2617b46b9cff57aaff143a82d
Oliver Y. Kui <oliverkui0324@outlook.com>
2024-02-19 22:22:41 +0800

    fix env

--- database/connect.js
+++ database/connect.js
@@ -2,8 +2,9 @@ const pgp = require("pg-promise")();
 
 const password = process.env.PASSWORD;
 const db = pgp(
-  // `postgresql://postgres:${password}@viaduct.proxy.rlwy.net:35556/railway`
-  `postgresql://postgres:${password}@postgres.railway.internal:5432/railway`
+  process.env.NODE_ENV === "production"
+    ? `postgresql://postgres:${password}@postgres.railway.internal:5432/railway`
+    : `postgresql://postgres:${password}@monorail.proxy.rlwy.net:27780/railway`
 );
 
 module.exports = db;
